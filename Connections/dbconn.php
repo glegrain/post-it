cde7b4ca992c234213d447ab1e58f31ba7af2a01
Guillaume Legrain <legrainguillaume@gmail.com>
2015-12-11 13:58:42 +0100

    removed php files to prepare for node.js repo import

--- Connections/dbconn.php
+++
@@ -1,7 +0,0 @@
-<?php
-#Connection to the database_dbconn
-$hostname_dbconn = "localhost";
-$database_dbconn = "postit";
-$username_dbconn = "dbuser";
-$password_dbconn = "g}&9DN&2Ex3.6V:&1_00";
-$dbconn = mysql_pconnect($hostname_dbconn, $username_dbconn, $password_dbconn) or trigger_error(mysql_error(),E_USER_ERROR); 
--- scripts/dbexport.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-*	Export SQL Database to a .sql file using system(mysqldump)
-*/
-require_once '../Connections/dbconn.php';
-
-$backupFile = '../database.sql';
-$command = "mysqldump --opt -h $hostname_dbconn -u $username_dbconn -p\"$password_dbconn\" postit > $backupFile"; //Make sure you can use the mysqldump command
-system($command);
