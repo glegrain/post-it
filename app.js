78f7c3a3110c1f6cea2aa5bf65e29dac65168a58
Guillaume Legrain <legrainguillaume@gmail.com>
2015-12-20 01:00:53 +0100

    added support for cross domain requests

--- app.js
+++ app.js
@@ -32,6 +32,21 @@ if ('development' == app.get('env')) {
 //app.get('/', routes.index);
 //app.get('/users', user.list); //list users
 
+// Add headers to allow cross domain requests
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');  // '*' all domains
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,  PUT, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/notes', notes.getNotes); // list notes
 app.post('/notes', notes.postNote); // create a new note
 app.put('/notes', function(req,res){throw new Error(); }); // error
