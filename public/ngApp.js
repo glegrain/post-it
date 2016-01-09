497b25b5e99f629d612cb7ce10d71540d0b51488
Guillaume Legrain <legrainguillaume@gmail.com>
2016-01-09 19:48:20 +0100

    added draggable directive

--- public/ngApp.js
+++ public/ngApp.js
@@ -62,3 +62,39 @@ ngApp.controller('NotesCtrl', function($scope, Note) {
     });
   }
 });
+
+ngApp.directive('draggable', function($document) {
+  return function(scope, element, attr) {
+    var startX = 0, startY = 0, x = 0, y = 0;
+    element.css({
+     position: 'relative',
+     border: '1px solid red',
+     backgroundColor: 'lightgrey',
+     cursor: 'pointer',
+     display: 'block',
+     width: '65px'
+    });
+    element.on('mousedown', function(event) {
+      // Prevent default dragging of selected content
+      event.preventDefault();
+      startX = event.screenX - element[0].offsetLeft;
+      startY = event.screenY - element[0].offsetTop;
+      $document.on('mousemove', mousemove);
+      $document.on('mouseup', mouseup);
+    });
+
+    function mousemove(event) {
+      y = event.screenY - startY;
+      x = event.screenX - startX;
+      element.css({
+        top: y + 'px',
+        left:  x + 'px'
+      });
+    }
+
+    function mouseup() {
+      $document.off('mousemove', mousemove);
+      $document.off('mouseup', mouseup);
+    }
+  };
+});
